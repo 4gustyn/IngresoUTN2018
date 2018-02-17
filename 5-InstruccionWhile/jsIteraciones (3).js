e3319e6bec544d9e8f2544907fafd83bf64d7827
4gustyn <36246903+4gustyn@users.noreply.github.com>
2018-02-16 22:13:36 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,10 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while(clave!="utn750")
+{
+    clave=prompt("contraseña incorrecta, reingrese la clave")
+}
+alert("te ganaste el quini")
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = prompt("ingrese un número entre 0 y 9.");
+
+	while(numero<=0&&numero<=10)
+	{
+		numero=prompt("incorrecto reingrese numero entre 0 y 10")
+	}
+	alert("correcto")
+
+
+	
 
 
 }//FIN DE LA FUNCIÓN
